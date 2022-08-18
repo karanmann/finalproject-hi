2f27dbbd6daf1ef25b4f1794578956a72cf1b7aa
Karan Mann <karan.mann@outlook.com>
2022-08-18 19:13:49 +0200

    Overlapping bug fixed with media queries

--- frontend/components/Hero/Tickers.js
+++ frontend/components/Hero/Tickers.js
@@ -4,223 +4,62 @@ import { TickerWrapper } from '../../styles/HomePageStyles';
 const Tickers = () => {
     return (
         <TickerWrapper>
-            <div className="marquee">
+            <div className="marquee col__1">
                 <div className="marquee__group">
-                    <img
-                        className="marquee__group_image"
-                        src="/images/01.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/02.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/03.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/04.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/05.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/06.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/07.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/08.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/09.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/10.png"
-                        alt="Hello"
-                    />
+                    <img src="/images/01.png" alt="Hello" />
+                    <img src="/images/02.png" alt="Hello" />
+                    <img src="/images/03.png" alt="Hello" />
+                    <img src="/images/04.png" alt="Hello" />
+                    <img src="/images/05.png" alt="Hello" />
+                    <img src="/images/06.png" alt="Hello" />
+                    <img src="/images/07.png" alt="Hello" />
+                    <img src="/images/08.png" alt="Hello" />
+                    <img src="/images/09.png" alt="Hello" />
+                    <img src="/images/10.png" alt="Hello" />
+                </div>
+            </div>
+            <div className="marquee__reverse col__2">
+                <div className="marquee__group">
+                    <img src="/images/01.png" alt="Hello" />
+                    <img src="/images/02.png" alt="Hello" />
+                    <img src="/images/03.png" alt="Hello" />
+                    <img src="/images/04.png" alt="Hello" />
+                    <img src="/images/05.png" alt="Hello" />
+                    <img src="/images/06.png" alt="Hello" />
+                    <img src="/images/07.png" alt="Hello" />
+                    <img src="/images/08.png" alt="Hello" />
+                    <img src="/images/09.png" alt="Hello" />
+                    <img src="/images/10.png" alt="Hello" />
                 </div>
             </div>
 
-            <div className="marquee__reverse">
+            <div className="marquee col__3">
                 <div className="marquee__group">
-                    <img
-                        className="marquee__group_image"
-                        src="/images/01.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/02.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/03.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/04.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/05.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/06.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/07.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/08.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/09.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/10.png"
-                        alt="Hello"
-                    />
+                    <img src="/images/01.png" alt="Hello" />
+                    <img src="/images/02.png" alt="Hello" />
+                    <img src="/images/03.png" alt="Hello" />
+                    <img src="/images/04.png" alt="Hello" />
+                    <img src="/images/05.png" alt="Hello" />
+                    <img src="/images/06.png" alt="Hello" />
+                    <img src="/images/07.png" alt="Hello" />
+                    <img src="/images/08.png" alt="Hello" />
+                    <img src="/images/09.png" alt="Hello" />
+                    <img src="/images/10.png" alt="Hello" />
                 </div>
             </div>
 
-            <div className="marquee">
+            <div className="marquee__reverse col__4">
                 <div className="marquee__group">
-                    <img
-                        className="marquee__group_image"
-                        src="/images/01.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/02.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/03.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/04.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/05.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/06.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/07.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/08.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/09.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/10.png"
-                        alt="Hello"
-                    />
-                </div>
-            </div>
-
-            <div className="marquee__reverse">
-                <div className="marquee__group">
-                    <img
-                        className="marquee__group_image"
-                        src="/images/01.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/02.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/03.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/04.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/05.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/06.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/07.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/08.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/09.png"
-                        alt="Hello"
-                    />
-                    <img
-                        className="marquee__group_image"
-                        src="/images/10.png"
-                        alt="Hello"
-                    />
+                    <img src="/images/01.png" alt="Hello" />
+                    <img src="/images/02.png" alt="Hello" />
+                    <img src="/images/03.png" alt="Hello" />
+                    <img src="/images/04.png" alt="Hello" />
+                    <img src="/images/05.png" alt="Hello" />
+                    <img src="/images/06.png" alt="Hello" />
+                    <img src="/images/07.png" alt="Hello" />
+                    <img src="/images/08.png" alt="Hello" />
+                    <img src="/images/09.png" alt="Hello" />
+                    <img src="/images/10.png" alt="Hello" />
                 </div>
             </div>
         </TickerWrapper>
