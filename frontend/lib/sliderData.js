6d05cbce9640ae91db4ee61e29213967e183dfec
Harry-Yates <hyates1@gmail.com>
2022-08-22 17:13:11 +0200

    Added two new slider videos

--- frontend/lib/sliderData.js
+++ frontend/lib/sliderData.js
@@ -1,22 +1,22 @@
 export const sliderData = [
-  {
-      title: '#Yoshimitsu',
-      video: 'https://user-images.githubusercontent.com/82885837/185081717-71d793c6-a486-40a4-910a-81f6dbb7d4ed.MOV',
-      url: 'https://www.google.com',
-  },
-  {
-      title: '#Keira Metz',
-      video: 'https://user-images.githubusercontent.com/82885837/184858875-9979d5b0-ec90-4ce9-a15c-f50bd9929a32.MOV',
-      url: 'https://www.google.com',
-  },
-  {
-      title: '#Yoshimitsu',
-      video: 'https://user-images.githubusercontent.com/82885837/185081717-71d793c6-a486-40a4-910a-81f6dbb7d4ed.MOV',
-      url: 'https://www.google.com',
-  },
-  {
-      title: '#Keira Metz',
-      video: 'https://user-images.githubusercontent.com/82885837/184858875-9979d5b0-ec90-4ce9-a15c-f50bd9929a32.MOV',
-      url: 'https://www.google.com',
-  },
+    {
+        title: '#Yoshimitsu',
+        video: 'https://user-images.githubusercontent.com/82885837/185081717-71d793c6-a486-40a4-910a-81f6dbb7d4ed.MOV',
+        url: 'https://www.google.com',
+    },
+    {
+        title: '#Ardyn',
+        video: 'https://user-images.githubusercontent.com/82885837/185955818-b5878aa6-f054-4ebf-855f-85989bacb405.MOV',
+        url: 'https://www.google.com',
+    },
+    {
+        title: '#Heihachi Mishima',
+        video: 'https://user-images.githubusercontent.com/82885837/185955968-38c6c705-6a90-4267-8455-e7dc2e10f077.MOV',
+        url: 'https://www.google.com',
+    },
+    {
+        title: '#Keira Metz',
+        video: 'https://user-images.githubusercontent.com/82885837/184858875-9979d5b0-ec90-4ce9-a15c-f50bd9929a32.MOV',
+        url: 'https://www.google.com',
+    },
 ];
