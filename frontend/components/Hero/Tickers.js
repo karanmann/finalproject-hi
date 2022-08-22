f91dfbf7feecbdad4c86b8a23cf59ac13d417ecd
Karan Mann <karan.mann@outlook.com>
2022-08-22 09:31:10 +0200

    Image Mapped

--- frontend/components/Hero/Tickers.js
+++ frontend/components/Hero/Tickers.js
@@ -1,81 +1,62 @@
 import Image from 'next/image';
 import { TickerWrapper } from '../../styles/HomePageStyles';
 
+
+// We can replace the below array with the new data from the context API
+const imagesArray = [
+    '/images/01.png',
+    '/images/02.png',
+    '/images/03.png',
+    '/images/04.png',
+    '/images/05.png',
+    '/images/06.png',
+    '/images/07.png',
+    '/images/08.png',
+    '/images/09.png',
+    '/images/10.png',
+]; 
+
+
 const Tickers = () => {
     return (
         <TickerWrapper>
             <div className="marquee col__1">
                 <div className="marquee__group">
-                    <img src="/images/01.png" alt="Hello" />
-                    <img src="/images/02.png" alt="Hello" />
-                    <img src="/images/03.png" alt="Hello" />
-                    <img src="/images/04.png" alt="Hello" />
-                    <img src="/images/05.png" alt="Hello" />
-                    <img src="/images/06.png" alt="Hello" />
-                    <img src="/images/07.png" alt="Hello" />
-                    <img src="/images/08.png" alt="Hello" />
-                    <img src="/images/09.png" alt="Hello" />
-                    <img src="/images/10.png" alt="Hello" />
+                    {imagesArray.map((image, index) => (
+                        <img key={index} src={image} alt="Hello" />
+                    ))}
                 </div>
             </div>
             <div className="marquee__reverse col__2">
                 <div className="marquee__group">
-                    <img src="/images/01.png" alt="Hello" />
-                    <img src="/images/02.png" alt="Hello" />
-                    <img src="/images/03.png" alt="Hello" />
-                    <img src="/images/04.png" alt="Hello" />
-                    <img src="/images/05.png" alt="Hello" />
-                    <img src="/images/06.png" alt="Hello" />
-                    <img src="/images/07.png" alt="Hello" />
-                    <img src="/images/08.png" alt="Hello" />
-                    <img src="/images/09.png" alt="Hello" />
-                    <img src="/images/10.png" alt="Hello" />
+                    {imagesArray.map((image, index) => (
+                        <img key={index} src={image} alt="Hello" />
+                    ))}
                 </div>
             </div>
 
             <div className="marquee col__3">
                 <div className="marquee__group">
-                    <img src="/images/01.png" alt="Hello" />
-                    <img src="/images/02.png" alt="Hello" />
-                    <img src="/images/03.png" alt="Hello" />
-                    <img src="/images/04.png" alt="Hello" />
-                    <img src="/images/05.png" alt="Hello" />
-                    <img src="/images/06.png" alt="Hello" />
-                    <img src="/images/07.png" alt="Hello" />
-                    <img src="/images/08.png" alt="Hello" />
-                    <img src="/images/09.png" alt="Hello" />
-                    <img src="/images/10.png" alt="Hello" />
+                    {imagesArray.map((image, index) => (
+                        <img key={index} src={image} alt="Hello" />
+                    ))}
                 </div>
             </div>
 
             <div className="marquee__reverse col__4">
                 <div className="marquee__group">
-                    <img src="/images/01.png" alt="Hello" />
-                    <img src="/images/02.png" alt="Hello" />
-                    <img src="/images/03.png" alt="Hello" />
-                    <img src="/images/04.png" alt="Hello" />
-                    <img src="/images/05.png" alt="Hello" />
-                    <img src="/images/06.png" alt="Hello" />
-                    <img src="/images/07.png" alt="Hello" />
-                    <img src="/images/08.png" alt="Hello" />
-                    <img src="/images/09.png" alt="Hello" />
-                    <img src="/images/10.png" alt="Hello" />
+                    {imagesArray.map((image,index) => (
+                        <img key={index} src={image} alt="Hello" />
+                    ))}
                 </div>
             </div>
             <div className="marquee col__5">
-                    <div className="marquee__group">
-                        <img src="/images/01.png" alt="Hello" />
-                        <img src="/images/02.png" alt="Hello" />
-                        <img src="/images/03.png" alt="Hello" />
-                        <img src="/images/04.png" alt="Hello" />
-                        <img src="/images/05.png" alt="Hello" />
-                        <img src="/images/06.png" alt="Hello" />
-                        <img src="/images/07.png" alt="Hello" />
-                        <img src="/images/08.png" alt="Hello" />
-                        <img src="/images/09.png" alt="Hello" />
-                        <img src="/images/10.png" alt="Hello" />
-                    </div>
+                <div className="marquee__group">
+                    {imagesArray.map((image, index) => (
+                        <img key={index} src={image} alt="Hello" />
+                    ))}
                 </div>
+            </div>
         </TickerWrapper>
     );
 };
