3dd9a5924b7c4acc71d35f491df32f6cc1e613ea
Harry-Yates <hyates1@gmail.com>
2022-08-30 14:50:04 +0200

    Added meta data and flavicon
    Co-authored-by: Karan Mann <karan.mann@outlook.com>

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,8 +7,58 @@ const Home = () => {
         <div>
             <Head>
                 <title>#MB2</title>
-                <meta name="MB2" content="Check em all out" />
+                <meta name="title" content="#MB2" />
+                <meta
+                    name="description"
+                    content="MB2 is a series of art vinyl toys, with limited quantities available. "
+                />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://mb2.vercel.app/" />
+                <meta property="og:title" content="#MB2" />
+                <meta
+                    property="og:description"
+                    content="MB2 is a series of art vinyl toys, with limited quantities available."
+                />
+                <meta
+                    property="og:image"
+                    content="https://user-images.githubusercontent.com/82885837/187440582-baa60537-da05-4b6e-8915-19bf8378630e.png"
+                />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta
+                    property="twitter:url"
+                    content="https://mb2.vercel.app/"
+                />
+                <meta property="twitter:title" content="#MB2" />
+                <meta
+                    property="twitter:description"
+                    content="MB2 is a series of art vinyl toys, with limited quantities available."
+                />
+                <meta
+                    property="twitter:image"
+                    content="https://user-images.githubusercontent.com/82885837/187440582-baa60537-da05-4b6e-8915-19bf8378630e.png"
+                />
                 <link rel="icon" href="/favicon.ico" />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest"></link>
             </Head>
 
             <main>
