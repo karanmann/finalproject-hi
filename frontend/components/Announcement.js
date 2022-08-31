bd05de061dbeaf5d356dca34a0711597d0d75f20
Karan <karan.mann@outlook.com>
2022-08-31 22:26:12 +0200

    Import error fixed

--- frontend/components/Announcement.js
+++ frontend/components/Announcement.js
@@ -4,7 +4,7 @@ import { Container } from '../styles/AnnouncementStyles'
 const Announcement = () => {
     return (
         <Container>
-        <h3>    75% SCALE VINYL ART SCULPTURES</h3>
+            <h3>    75% SCALE VINYL ART SCULPTURES</h3>
         </Container>
     )
 }
